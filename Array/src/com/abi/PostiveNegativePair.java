6ff159337348a339173dcea0c76391a7a04abad2
abasit <abasit@ebay.com>
2017-10-16 12:29:38 +0530

    Pairs with Positive Negative values

---
+++ Array/src/com/abi/PostiveNegativePair.java
@@ -0,0 +1,43 @@
+/*
+ * http://practice.geeksforgeeks.org/problems/pairs-with-positive-negative-values/0
+ */
+package com.abi;
+
+import java.util.Scanner;
+
+public class PostiveNegativePair {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int T= sc.nextInt();
+
+		while(T-- > 0){
+			int N = sc.nextInt();
+			boolean pos[] = new boolean[101];
+			boolean neg[] = new boolean[101];
+			for (int i = 0; i < N; i++) {
+				int n = sc.nextInt();				
+				if(n < 0){
+					neg[n*-1] = true;
+				}else{
+					pos[n] = true;
+				}
+			}
+			
+			boolean isAnyResult  = false;
+			for (int i = 0; i < 101; i++) {
+				if(pos[i] && neg[i]){
+					isAnyResult = true;
+					System.out.print("-" + i + " " + i + " ");
+				}				
+			}
+			if(!isAnyResult)
+				System.out.print(0);
+		}
+
+		sc.close();
+
+	}
+
+}
